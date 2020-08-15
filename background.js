4117350843675f1a096432e37ae9c28be567a36b
Leha <song.vo@yandex.ru>
2020-08-15 14:41:10 +0700

    The application recognizes the active page

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
-console.log('from background');
+chrome.tabs.onActivated.addListener(tab => {
+  chrome.tabs.get(tab.tabId, current_tab_info => {
+    console.log(current_tab_info.url);
+  })
+})
+
+// chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected'))
