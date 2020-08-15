5d565a31faa8ba22ff0428f5cd6c41d9421b39ea
Leha <song.vo@yandex.ru>
2020-08-15 17:27:07 +0700

    App can match domain

--- background.js
+++ background.js
@@ -1,7 +1,31 @@
-chrome.tabs.onActivated.addListener(tab => {
-  chrome.tabs.get(tab.tabId, current_tab_info => {
-    console.log(current_tab_info.url);
-  })
-})
+const intervalHour = 60000 * 60
+let domain = {}
+const getDomainList = fetch('http://www.softomate.net/ext/employees/list.json ')
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            domain = data
+          })
 
-// chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log('i injected'))
+setTimeout(() => {
+  console.log(domain);
+}, 1000);
+
+setInterval(() => {
+  getDomainList()
+}, intervalHour);
+
+chrome.tabs.onActivated.addListener(tab => {
+  let activeTab = '';
+  chrome.tabs.get(tab.tabId, current_tab_info => {
+    for (let i = 0; i < domain.length; i++) {
+      let domainName = domain[i].name;
+      let regex = new RegExp(`^https:\/\/www\.${domainName}`)
+      if (regex.test(current_tab_info.url)) {
+        chrome.tabs.executeScript(null, { file: './foreground.js' }, )
+      }
+      }
+    }
+  )
+})
