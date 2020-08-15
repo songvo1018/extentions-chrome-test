d1688da7931596b9e24c602a133e676df4d69acf
Alexey Nosov <52455378+songvo1018@users.noreply.github.com>
2020-08-15 22:54:36 +0700

    app closed message after 3 and more upload page, but reqired check handle close message

--- content-script.js
+++ content-script.js
@@ -1,52 +1,65 @@
 chrome.storage.local.get(['openCounter'], result => {
-  console.log(result.openCounter);
-  if ( result.openCounter > 3) {
-    block.setAttribute("class", "hide");
+  if (result.openCounter < 4) {
+    initialize()
   }
 })
 
-chrome.storage.local.get(['opened'], result => {
-  if (!result.opened) {
-    block.removeElement()
-  }
-})
-
-let block = document.createElement("div");
-block.setAttribute("class", "block");
-
-let closeButton = document.createElement("div")
-closeButton.setAttribute("id", "closeButton")
-closeButton.textContent = "+"
-
-let message = document.createElement("span")
-message.textContent = "Этот параграф был добавлен приложением.";
-message.setAttribute("id", "message");
-
-block.appendChild(closeButton);
-block.appendChild(message);
-document.body.appendChild(block);
-
-closeButton.addEventListener("click", () => {
+const initialize = () => {
   
-  block.setAttribute("class", "hide");
+  let block = document.createElement("div");
+  block.setAttribute("class", "block");
 
-  chrome.storage.local.set(
-    {
-      opened: false
+  let closeButton = document.createElement("div")
+  closeButton.setAttribute("id", "closeButton")
+  closeButton.textContent = "+"
+
+  let message = document.createElement("span")
+  message.textContent = "Этот параграф был добавлен приложением.";
+  message.setAttribute("id", "message");
+
+  block.appendChild(closeButton);
+  block.appendChild(message);
+  document.body.appendChild(block);
+
+  // chrome.storage.local.get(['openCounter'], result => {
+  //   console.log(result.openCounter);
+  //   if ( result.openCounter > 3) {
+  //     block.setAttribute("class", "hide");
+  //   }
+  // })
+  
+  // Проверить скрываемость сообщения после назатия на крестик
+  chrome.storage.local.get(['opened'], result => {
+    if (!result.opened) {
+      block.removeElement()
     }
-  );
-})
+  })
+
+  closeButton.addEventListener("click", () => {
+    
+    block.setAttribute("class", "hide");
+
+    chrome.storage.local.set(
+      {
+        opened: false
+      }
+    );
+  })
+}
+
 
 chrome.storage.local.get(['openCounter'], result => {
+  if (result.openCounter < 4) {
+    console.log('open counter:', result.openCounter );
 
-  console.log('open counter:', result.openCounter );
-
-  chrome.storage.local.set(
-    {
-      openCounter: result.openCounter+1,
-      opened: true
-    }
-  );
+    chrome.storage.local.set(
+      {
+        openCounter: result.openCounter+1
+        // openCounter: 0,
+        // opened: true
+      }
+    );
+  }  
 });
 
 
